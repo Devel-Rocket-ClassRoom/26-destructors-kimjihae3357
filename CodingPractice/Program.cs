3a2f33e7ca5ca1f2b73260177f324acfb8434be2
bmj1644035 <bmj1644035@gmail.com>
2026-02-25 18:03:04 +0900

    26챕터 과제제출

--- CodingPractice/Program.cs
+++ CodingPractice/Program.cs
@@ -1,4 +1,63 @@
 using System;
+using System.Reflection.PortableExecutable;
 
-// README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+// 1
+
+TestObject obj1 = new TestObject(1);
+TestObject obj2 = new TestObject(2);
+
+obj1 = null;
+obj2 = null;
+
+Console.WriteLine("GC 실행 전");
+
+GC.Collect();
+GC.WaitForPendingFinalizers();
+
+Console.WriteLine("실행 후");
+
+class TestObject
+{
+    private int _id;
+
+    public TestObject(int id)
+    {
+        _id = id;
+        Console.WriteLine($"객체 {_id} 생성");
+    }
+    ~TestObject()
+    {
+        Console.WriteLine($"객체 {_id} 소멸");
+    }
+}
+
+// 2
+
+Console.WriteLine("v프로그램 시작");
+
+Character hero = new Characrer("용사");
+hero.Attack();
+hero = null;
+
+Console.WriteLine("GC 실행");
+GC.Collect();
+GC.WaitForPendingFinalizers();
+
+Console.WriteLine("프로그램 종료");
+
+class Character
+{
+    private string _name;
+    
+        public Character (string name)
+    {
+        _name = name;
+        Console.WriteLin($"[1] _name)"
+    }
+
+    public void Attack()
+    {
+        Console.WriteLine($"[2] {_name} 공격");
+    }
+
+}
